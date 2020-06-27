03808cebaf2fddde19332206254f27063d2550dc
wutong <wutongtoby@gmail.com>
2020-06-27 23:25:58 +0800

    t

--- main.cpp
+++ main.cpp
@@ -2,41 +2,273 @@
 #include "bbcar.h"
 #include "bbcar_rpc.h"
 
+#define LEFT 1
+#define RIGHT 0
+#define FORWARD 1
+#define BACKWARD 0
+
+// Servo
 Ticker servo_ticker;
 PwmOut pin13(D13), pin10(D10);
-Serial xbee(D1, D0); // Tx, Rx
 BBCar car(pin13, pin10, servo_ticker);
+
+// Encoder
+DigitalIn pin4(D4), pin5(D5);
+Ticker encoder_ticker0;
 Ticker encoder_ticker1;
-Ticker encoder_ticker2;
+parallax_encoder en1(pin4, encoder_ticker1);
+parallax_encoder en0(pin5, encoder_ticker0);
+
+// Ping
 DigitalInOut pin6(D6);
 parallax_ping ping(pin6);
 
-//DigitalIn pin3(D3);
-//DigitalIn pin4(D4);
+// Xbee
+RawSerial xbee(D1, D0); // Tx, Rx
 
-// just wait for rpc call from the xbee!
+// OpenMv
+Serial OpenMv(D12, D11); // Tx, Rx
+
+// LED
+DigitalOut Led(LED1);
+
+void identify_matrix(void); 
+void identify_picture(void);
+void identify_object(void); 
+
+bool other_mission = false;
+void log(void);
+Thread t;
+char log_message;
+
+void ping_WalkUntil(float distance);
+void lr_turn(bool Is_left);
+
 int main(void) 
 {
-    
+    // set the baud rate for both
+    xbee.baud(9600);
+    OpemMv.baud(9600);
+    t.start(&log);
+
+    // set the LED
+    Led = 1;
+
     // for D13 
-    double pwm_table0[] = {-50, -40, -30, -20, -10, 0, 10, 20, 30, 40, 50};
-    double speed_table0[] = {-20.177, -16.189, -11.484, -5.742, -0.799, 0, 4.785, 10.288, 15.551, 20.974, 25.440};     
+    double pwm_table0[] = {-40, -32, -24, -16, -8, 0, 8, 16, 24, 32, 40};
+    double speed_table0[] = {-19.059, -14.355, -10.367, -5.343, -1.754, 0, 2.153, 7.018, 11.165, 16.109, 20.096};     
     // for D10
-    double pwm_table1[] = {-150, -120, -90, -60, -30, 0, 30, 60, 90, 120, 150};
-    double speed_table1[] = {-16.928, -15.950, -15.072, -12.281, -6.061, 0, 5.822, 11.723, 14.913, 16.02, 16.428};
+    double speed_table1[] = {-16.108, -15.312, -13.557, -9.950, -4.227, 0, 4.944, 10.367, 13.956, 15.631, 16.348};
+    double pwm_table1[] = {-120, -96, -72, -48, -24, 0, 24, 48, 72, 96, 120};
 
     // first and fourth argument : length of table
     car.setCalibTable(11, pwm_table0, speed_table0, 11, pwm_table1, speed_table1);
 
-    // let both servo go straight with the speed, 
+    // start 
     
+    // get the information of matrix at 25cm
+    ping_WalkUntil(25, FORWARD);
+    identify_matrix();
+    log_message = 'F';
+    wait(1);
     
+    // go until at the front of wall
+    ping_WalkUntil(5, FORWARD);
+    log_message = 'F';
+    lr_turn(LEFT);
+    log_message = 'L';
+
+    // go until at the target place, then turn right + go backward
+    ping_WalkUntil(66, FORWARD);
+    log_message = 'F';
+    lr_turn(RIGHT);
+    log_message = 'R';
+    ping_WalkUntil(55, BACKWARD);
+    log_message = 'B';
+
+    // go forward to identify the picture
+    ping_WalkUntil(47.5, FORWARD);
+    log_message = 'F';
+    identify_picture();
+    wait(1);
+
+    // go to the next mission
+    ping_WalkUntil(5, FORWARD);
+    log_message = 'F';
+    lr_turn(RIGHT);
+    log_message = 'R';
+    ping_WalkUntil(30, FORWARD);
+    log_message = 'F';
+    lr_turn(RIGHT);
+    log_message = 'R';
+    ping_WalkUntil(5, FORWARD);
+    log_message = 'F';
+    lr_turn(RIGHT);
+    ping_WalkUntil(80, FORWARD);
+    log_message = 'F';
+    lr_turn(RIGHT);
+    log_message = 'R';
+    // need to be careful since the distance may be wrong due to the slope object
+    ping_WalkUNtil(35, FORWARD);
+    log_message = 'F';
+    identify_object();
+
+    // go to the end
+    lr_turn(LEFT);
+    log_message = 'L';
+    ping_WalkUntil(25, FORWARD);
+    log_message = 'F';
+    lr_turn(LEFT);
+    log_message = 'L';
+    ping_WalkUntil(5, FORWARD);
+    log_message = 'F';
+    lr_turn(RIGHT);
+    log_message = 'R';
+    pint_WalkUntil(5, FORWARD);
+    log_message = 'F';
+    lr_turn(RIGHT);
+    log_message = 'R';
     car.goStraightCalib(10);
+    log_message = 'F';
+    wait(10);
+    car.stop();
+
+   /*
+    for (int i = 0; i < 4; i ++) {
+        car.goStraightCalib(10);
+        wait(4.5);
+        car.servo0.set_speed_by_cm(10);
+        car.servo1.set_speed_by_cm(10);
+        car.controlWheel();
+        wait(1.12);
+    }
+    car.stop();
+    */
+}
+
+void ping_WalkUntil(float distance, bool Is_Forward)
+{
+    if (Is_forward)
+        car.goStraightCalib(10);
+    else 
+        car.goStraightCalib(-10);
+
     while (1) {
-        if (float(ping) <= 15)
+        if (float(ping) <= distance)
             break;
         wait(0.1);
     };
-    
     car.stop();
+}
+
+void lr_turn(bool IsLeft)
+{
+    if (Is_Left) {
+        car.servo0.set_speed_by_cm(-10);
+        car.servo1.set_speed_by_cm(-10);
+        car.controlWheel();
+        wait(1.12);
+    }
+    else {
+        car.servo0.set_speed_by_cm(10);
+        car.servo1.set_speed_by_cm(10);
+        car.controlWheel();
+        wait(1.12);
+    }
+    car.stop();
+}
+
+void log(void) {
+    int count = 0;
+    while (1) {
+        if (!other_mission) {
+            xbee.printf("%c\r\n", log_message, count++);
+            wait(1.0);
+        }
+    }
+}
+
+void identify_matrix(void)
+{
+    other_mission = true;
+    char s[20];
+    sprintf(s, "data_matrix");
+    OpenMv.puts(s);
+    wait(5);
+    sprintf(s, "stop");
+    OpenMv.puts(s);
+    wait(1);
+
+    if (OpenMv.readable()) {
+        char temp;
+        char rev[20];
+        int count = 0;
+        
+        while (1) {
+            temp = uart.getc();
+            if (temp != '\r') {
+                rev[counter++] = temp;
+            }
+            else {
+                break;
+            }
+        }
+        for (int i = 0; i < count; i++) {
+            xbee.putc(rev[i]);
+        }
+        xbee.printf("\r\n");
+    }
+
+    // mission complete
+    for (int i = 0; i < 5; i++) {
+        Led = 0;
+        wait(0.2);
+        Led = 1;
+        wait(0.2);
+    }
+    other_mission = false;
+}
+
+void identify_picture(void)
+{
+    other_mission = true;
+    
+    char s[20];
+    sprintf(s, "image_classification");
+    OpenMv.puts(s);
+    wait(5);
+    // will get only one number
+    if (uart.readable()) {
+        char recv = OpenMv.getc();
+        xbee.putc(recv);
+        xbee.printf("\r\n");
+    }
+    // mission complete
+    for (int i = 0; i < 5; i++) {
+        Led = 0;
+        wait(0.2);
+        Led = 1;
+        wait(0.2);
+    }
+    other_mission = false;
+}
+void identify_get(void)
+{
+  mission_gap = 1;
+  redLED = 0;
+  // send quest
+  char s[10];
+  sprintf(s, "identify");
+  uart.puts(s);
+  wait(5);
+  // fetch information
+  if (uart.readable())
+  {
+    char recv = uart.getc();
+    xbee.printf("80661\r\n");
+    xbee.putc(recv);
+    xbee.printf("\r\n");
+  }
+  mission_gap = 0;
+  redLED = 1;
 }
---
+++ main.py
@@ -0,0 +1,58 @@
+# to identify number by outside call
+import pyb
+import sensor, image, time, os, tf, math
+
+uart = pyb.UART(3,9600,timeout_char=1000)
+uart.init(9600,bits=8,parity = None, stop=1, timeout_char=1000)
+tmp = ""
+
+def image_classification():
+    sensor.reset()                         # Reset and initialize the sensor.
+    sensor.set_pixformat(sensor.RGB565)    # Set pixel format to RGB565 (or GRAYSCALE)
+    sensor.set_framesize(sensor.QVGA)      # Set frame size to QVGA (?x?)
+    sensor.set_windowing((240, 240))       # Set 240x240 window.
+    sensor.skip_frames(time=2000)          # Let the camera adjust.
+
+    labels = ['3', '4', '0', 'other']
+
+    img = sensor.snapshot().save("number.jpg")
+
+    for obj in tf.classify('/model_demo.tflite',img, min_scale=1.0, scale_mul=0.5, x_overlap=0.0, y_overlap=0.0):
+        img.draw_rectangle(obj.rect())
+        img.draw_string(obj.x()+3, obj.y()-1, labels[obj.output().index(max(obj.output()))], mono_space = False)
+    return labels[obj.output().index(max(obj.output()))]
+
+def data_matrix():
+    sensor.reset()
+    sensor.set_pixformat(sensor.RGB565)
+    sensor.set_framesize(sensor.QVGA)
+    sensor.skip_frames(time = 2000)
+    sensor.set_auto_gain(False)  # must turn this off to prevent image washout...
+    sensor.set_auto_whitebal(False)  # must turn this off to prevent image washout...
+    
+    img = sensor.snapshot()
+    img.lens_corr(1.8) # strength of 1.8 is good for the 2.8mm lens.
+
+    matrices = img.find_datamatrices()
+    for matrix in matrices:
+        return str((180 * matrix.rotation()) / math.pi)
+    return "no_matrix"
+
+last_angle = ""
+
+while(1):
+    a = uart.readline()
+    if a is not None:
+        tmp += a.decode()
+
+    if tmp == "image_classification":
+        tmp = ""
+        label = image_classification()
+        uart.write(label.encode())
+        
+    if tmp == "data_matrix":
+        last_angle = data_matrix() + "\r"
+        
+    if tmp == "stop":
+        tmp = ""
+        uart.write(last_angle.encode())
